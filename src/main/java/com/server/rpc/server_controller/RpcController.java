85bf2e944a9087a1c1152b29d4445a0bd6f4c78b
EugeneLeonidovich <7k6p1wr@gmail.com>
2021-03-23 15:34:09 +0200

    Commit

--- src/main/java/com/server/rpc/server_controller/RpcController.java
+++ src/main/java/com/server/rpc/server_controller/RpcController.java
@@ -1,5 +1,6 @@
 package com.server.rpc.server_controller;
 
+import RequestMethods.Methods;
 import com.server.rpc.DAOAddress.AddressDAO;
 import com.server.rpc.address.AddressService;
 import com.server.rpc.data.JsonRpcError;
@@ -8,11 +9,13 @@ import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import com.server.rpc.Constants.Text;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/")
@@ -37,20 +40,20 @@ public class RpcController {
         JSONObject json = new JSONObject(obj);
         Map<String, Object> resp = new HashMap<>();
         resp.put(JsonRpcKeys.JSON_RPC_KEY, JsonRpcKeys.JSON_RPC_VERSION);
-        resp.put(JsonRpcKeys.ID_KEY, getId(json));
+        resp.put(JsonRpcKeys.ID_KEY, Methods.getId(json));
         if (JsonRpcError.requestIsValid(json)) {
             switch (json.getString("method")){
                 case "generateInteger":
-                    resp.put(JsonRpcKeys.RESULT_KEY, generateInteger());
+                    resp.put(JsonRpcKeys.RESULT_KEY, Methods.generateInteger());
                     break;
                 case "getText":
-                    resp.put(JsonRpcKeys.RESULT_KEY, getText());
+                    resp.put(JsonRpcKeys.RESULT_KEY, Methods.getText());
                     break;
                 case "getVersion":
-                    resp.put(JsonRpcKeys.RESULT_KEY, getVersion());
+                    resp.put(JsonRpcKeys.RESULT_KEY, Methods.getVersion());
                     break;
                 case "get_peer_list":
-                    resp.put(JsonRpcKeys.RESULT_KEY, get_peer_list());
+                    resp.put(JsonRpcKeys.RESULT_KEY, Methods.get_peer_list());
                     break;
                 default:
                     resp.put(JsonRpcKeys.RESULT_KEY, -1);
@@ -65,29 +68,6 @@ public class RpcController {
         return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
     }
 
-
-    private Object getId(JSONObject obj){
-        if (obj.isNull(JsonRpcKeys.ID_KEY))
-            return null;
-        return obj.get(JsonRpcKeys.ID_KEY);
-    }
-    private String getText(){
-        return Text.text.get(new Random().nextInt(Text.text.size()));
-    }
-
-    private Integer generateInteger(){
-        return new Random().nextInt(10);
-    }
-
-    private String getVersion(){
-        return "0.1";
-    }
-
-    private List<String> get_peer_list(){
-        AddressDAO addressDAO = new AddressService();
-        List<String> ip = addressDAO.getAll();
-        return ip;
-    }
 
     private void addIp(String ip){
         AddressDAO addressDAO = new AddressService();
